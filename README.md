c3d9b94be20da7250e71bfee642b8006d31c269e
lcgogo <lcgogo123@163.com>
2020-12-30 18:07:46 +0800

    install_as_systemd.sh

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # spec_login_alert
 send alert to pageduty or email if spec one login from last log
+
+```shell
+git clone <this_repo>
+cd this_repo
+cp local_settings.py.example local_settings.py
+# add config
+vi local_settings.py
+./install_as_systemd.sh
+```
---
+++ install_as_systemd.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+SHELL_FOLDER=$(cd "$(dirname "$0")";pwd)
+PROCESS=spec_login_alert
+DESCRIPTION="send alert when new user login"
+
+if [ ! -f "${SHELL_FOLDER}/${PROCESS}.py" ];then
+  echo Not found "${SHELL_FOLDER}/${PROCESS}.py Exit Now."
+  exit 1
+else
+  chmod +x "${SHELL_FOLDER}/${PROCESS}.py"
+fi
+
+echo "==== Run ${DESCRIPTION} in systemd ===="
+cat > /tmp/${PROCESS}.service << EOF
+[Service]
+User=${USER}
+Group=${USER}
+ExecStart=${SHELL_FOLDER}/${PROCESS}.py
+Restart=always
+StandardOutput=syslog
+StandardError=inherit
+
+[Install]
+WantedBy=multi-user.target
+
+[Unit]
+Description=${DESCRIPTION}
+After=network.target
+EOF
+sudo mv /tmp/${PROCESS}.service /lib/systemd/system/${PROCESS}.service
+oldpid=`ps -ef | grep "${PROCESS}" | grep -v grep | awk '{print $2}'`
+if [ ! -z "${oldpid}" ];then
+  echo Old ${PROCESS} is running. Stop it now.
+  sudo systemctl stop ${PROCESS}.service
+fi
+sudo systemctl daemon-reload
+sudo systemctl start ${PROCESS}.service
+sudo systemctl enable ${PROCESS}.service
+sleep 2
+sudo systemctl status ${PROCESS}.service
